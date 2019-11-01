1758c8c7604a8758778ad30d567c1c6950668d17
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-11-01 17:42:17 -0500

    products controller show

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -5,4 +5,9 @@ class Api::V1::ProductsController < ApplicationController
         @products = Product.all
         render json: @products.to_json()
     end
+
+    def show
+        @product = Product.find_by(id: params[:id])
+        render json: @product, except: [:updated_at, :created_at]
+    end
 end
