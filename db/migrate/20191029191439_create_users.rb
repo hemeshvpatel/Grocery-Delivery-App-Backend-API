518f5321c0aa0f322c6a7d40dbaf9666db1ebb80
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-11-06 11:44:28 -0600

    added cart and cartitems models/controllers and updated db:

--- db/migrate/20191029191439_create_users.rb
+++ db/migrate/20191029191439_create_users.rb
@@ -7,6 +7,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :password_digest
       t.string :email
       t.boolean :admin, default: false
+      t.integer :current_order, default: []
 
       t.timestamps
     end
